19c38356cc55fa292171f122ce3b2e04826eb7f3
stepanferubko <stepan.ferubko@flexshore.com>
2019-11-14 00:26:33 +0200

    added cards to content

--- src/app/core-material.module.ts
+++ src/app/core-material.module.ts
@@ -31,8 +31,27 @@ import {
   MatTooltipModule,
   MatAutocompleteModule
 } from "@angular/material";
+import {A11yModule} from '@angular/cdk/a11y';
+import {BidiModule} from '@angular/cdk/bidi';
+import {ObserversModule} from '@angular/cdk/observers';
+import {OverlayModule} from '@angular/cdk/overlay';
+import {PlatformModule} from '@angular/cdk/platform';
+import {PortalModule} from '@angular/cdk/portal';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {CdkStepperModule} from '@angular/cdk/stepper';
+import {CdkTableModule} from '@angular/cdk/table';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 const MATERIAL_MODULES = [
+  A11yModule,
+  BidiModule,
+  ObserversModule,
+  OverlayModule,
+  PlatformModule,
+  PortalModule,
+  ScrollingModule,
+  CdkStepperModule,
+  CdkTableModule,
   MatButtonModule,
   MatButtonToggleModule,
   MatCheckboxModule,
@@ -62,7 +81,8 @@ const MATERIAL_MODULES = [
   MatTabsModule,
   MatToolbarModule,
   MatTooltipModule,
-  MatAutocompleteModule
+  MatAutocompleteModule,
+  FlexLayoutModule
 ];
 
 @NgModule({
